856dcc328712e23bee31d4bb92e26fa288ec3383
Gleb Nichyparovich <gleb.nichyparovich@iskra-mehanizmi.si>
2024-01-31 08:52:05 +0100

    dev - WIP

--- src/DiffServiceApp.Application/Extensions/Base64StringValidator.cs
+++ src/DiffServiceApp.Application/Extensions/Base64StringValidator.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Buffers.Text;
 
 namespace DiffServiceApp.Application.Extensions;
 
@@ -6,19 +6,6 @@ internal static class Base64StringValidator
 {
     public static bool IsValidBase64String(this string base64String)
     {
-        // Check if the string is null or empty
-        if (string.IsNullOrEmpty(base64String))
-        {
-            return false;
-        }
-
-        // Check if length is a multiple of 4
-        if (base64String.Length % 4 != 0)
-        {
-            return false;
-        }
-
-        // Regular expression to check if it contains only valid Base64 characters
-        return Regex.IsMatch(base64String, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
+        return Base64.IsValid(base64String);
     }
 }
